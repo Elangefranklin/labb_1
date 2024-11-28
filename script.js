b391257d0aa8a017100106735792e6c8fc658c8e
Elange Franklin <114111024+Elangefranklin@users.noreply.github.com>
2024-11-28 15:21:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Variabel 1: Checkboxen
+// Hämta med querySelector
+const divStyleCheckbox = document.querySelector("input[type='checkbox']");
+
+// Variabel 2: Alla textfält
+// Hämta med getElementsByClassName
+const textFields = document.getElementsByClassName("textfield");
+
+// Variabel 3: Knappen
+// Hämta med querySelector (första knappen på sidan)
+const removeButton = document.querySelector("button");
+
+// Variabel 4: Div-elementet
+// Hämta med getElementById
+const emptyDiv = document.getElementById("emptyDiv");
